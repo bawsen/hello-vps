ee987eae275973474a2a306508c61d30f41d45e9
linusidk <linusidk@stepintomyshower.com>
2017-09-29 21:29:12 +0200

    Start of google-vps

---
+++ google-vps.sh
@@ -0,0 +1,9 @@
+# So what cost this gem  and where can i find it you ask
+# Lets go google.
+# First go make a brandnew google accoint
+# Sign up here: https://cloud.google.com/.
+# When done you should have 300$ as a sign-up bonus..:)
+# goto https://console.cloud.google.com
+# and follow the manual when shows u aroud
+#What we want is 3 small servers running ubuntu.. thease are our infrastrcture.
+#reamber to set a new public ip adress to your servers.(a workaround is to deplyo one server in each zone(asia-east1,australia-southeast1,europe-west1,us-central1), bc alimit on  max one ip per zone
